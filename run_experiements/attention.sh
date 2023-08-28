b2b2d41baac1cced373dffabdbfe8b183b08cca8
Josh <zhaoxia2@ualberta.ca>
2023-08-28 14:41:31 -0600

    Create atttention.sh file to run experiments in batch

---
+++ run_experiements/attention.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Run main.py with different attention_mode arguments
+
+# Run with 'vv' attention_mode
+python /home/zhaoxiang/CLIP_AD/WinClip_zzx/main.py --attention_mode vv
+
+# Run with 'v' attention_mode
+python /home/zhaoxiang/CLIP_AD/WinClip_zzx/main.py --attention_mode v
+
+# Run with 'qkv' attention_mode
+python /home/zhaoxiang/CLIP_AD/WinClip_zzx/main.py --attention_mode qkv
