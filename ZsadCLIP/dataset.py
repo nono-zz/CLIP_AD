671b998563af228fa836fe439e50a0a864567726
Josh <zhaoxia2@ualberta.ca>
2023-09-23 18:43:25 -0600

    Run zero shot clip on loco dataset

--- ZsadCLIP/dataset.py
+++ ZsadCLIP/dataset.py
@@ -161,6 +161,137 @@ class MVTecDataset(data.Dataset):
 				'img_path': os.path.join(self.root, img_path)}
 
 
+class MvTecLocoDataset(data.Dataset):
+	def __init__(self, root, transform, target_transform, aug_rate=-1, mode='test', k_shot=0, save_dir=None, obj_name=None):
+		self.root = root
+		self.transform = transform
+		self.target_transform = target_transform
+		self.aug_rate = aug_rate
+
+		self.data_all = []
+		# meta_info = json.load(open(f'{self.root}/meta.json', 'r'))
+		# name = self.root.split('/')[-1]
+		# meta_info = meta_info[mode]
+		self.tot_class_names = os.listdir(root)
+		self.tot_class_names = [item for item in self.tot_class_names if os.path.isdir(os.path.join(root, item))]
+
+
+		if mode == 'train':
+			self.cls_names = [obj_name]
+			save_dir = os.path.join(save_dir, 'k_shot.txt')
+		else:
+			self.cls_names = self.tot_class_names
+		for cls_name in self.cls_names:
+			if mode == 'train':
+				cls_train_dir = os.path.join(root, cls_name, 'train/good')
+				data_tmp = [os.path.join(cls_train_dir, img_name) for img_name in os.listdir(cls_train_dir)]
+				indices = torch.randint(0, len(data_tmp), (k_shot,))
+				for i in range(len(indices)):
+					self.data_all.append(data_tmp[indices[i]])
+					with open(save_dir, "a") as f:
+						f.write(data_tmp[indices[i]]['img_path'] + '\n')
+			# test mode: load all test data images
+			else:
+				for cls_test_category in os.listdir(os.path.join(root, cls_name, 'test')):
+					self.data_all.extend([os.path.join(root, cls_name, 'test', cls_test_category, img_name) 
+                           for img_name in os.listdir(os.path.join(root, cls_name, 'test', cls_test_category))])       
+				# self.data_all.extend(meta_info[cls_name])
+		self.length = len(self.data_all)
+
+	def __len__(self):
+		return self.length
+
+	def get_cls_names(self):
+		return self.cls_names
+
+	def combine_img(self, cls_name):
+		img_paths = os.path.join(self.root, cls_name, 'test')
+		img_ls = []
+		mask_ls = []
+		for i in range(4):
+			defect = os.listdir(img_paths)
+			random_defect = random.choice(defect)
+			files = os.listdir(os.path.join(img_paths, random_defect))
+			random_file = random.choice(files)
+			img_path = os.path.join(img_paths, random_defect, random_file)
+			mask_path = os.path.join(self.root, cls_name, 'ground_truth', random_defect, random_file[:3] + '_mask.png')
+			img = Image.open(img_path)
+			img_ls.append(img)
+			if random_defect == 'good':
+				img_mask = Image.fromarray(np.zeros((img.size[1], img.size[0])), mode='L')
+			else:
+				img_mask = np.array(Image.open(mask_path).convert('L')) > 0
+				img_mask = Image.fromarray(img_mask.astype(np.uint8) * 255, mode='L')
+			mask_ls.append(img_mask)
+		# image
+		image_width, image_height = img_ls[0].size
+		result_image = Image.new("RGB", (2 * image_width, 2 * image_height))
+		for i, img in enumerate(img_ls):
+			row = i // 2
+			col = i % 2
+			x = col * image_width
+			y = row * image_height
+			result_image.paste(img, (x, y))
+
+		# mask
+		result_mask = Image.new("L", (2 * image_width, 2 * image_height))
+		for i, img in enumerate(mask_ls):
+			row = i // 2
+			col = i % 2
+			x = col * image_width
+			y = row * image_height
+			result_mask.paste(img, (x, y))
+
+		return result_image, result_mask
+
+	def load_data_info(self, img_path):
+		anomaly_type = img_path.split('/')[-2]
+		cls_name = img_path.split('/')[-4]
+
+		if anomaly_type == 'good':
+			mask_path = None
+		else:
+			path_components = img_path.split('/')
+			path_components[-3] = 'ground_truth'
+			path_components[-1] = path_components[-1].replace('.png', '')
+			# path_components
+			mask_dir_path = "/".join(path_components)
+			mask_files = os.listdir(mask_dir_path)
+			if len(mask_files) == 1:
+				mask_path = os.path.join(mask_dir_path, mask_files[0])
+			else:
+				print('two gt_masks are found!!!')
+				mask_path = os.path.join(mask_dir_path, mask_files[0])		# leave it here for future changes
+		return mask_path, cls_name, anomaly_type
+
+	def __getitem__(self, index):
+		img_path = self.data_all[index]
+		# img_path, mask_path, cls_name, specie_name, anomaly = data['img_path'], data['mask_path'], data['cls_name'], \
+		# 													  data['specie_name'], data['anomaly']
+		mask_path, cls_name, anomaly_type = self.load_data_info(img_path)
+  
+		random_number = random.random()
+		if random_number < self.aug_rate:
+			img, img_mask = self.combine_img(cls_name)
+		else:
+			img = Image.open(os.path.join(self.root, img_path))
+			if anomaly_type == 'good':
+				img_mask = Image.fromarray(np.zeros((img.size[0], img.size[1])), mode='L')
+				anomaly = 0
+			else:
+				img_mask = np.array(Image.open(mask_path).convert('L')) > 0
+				img_mask = Image.fromarray(img_mask.astype(np.uint8) * 255, mode='L')
+				anomaly = 1
+		# transforms
+		img = self.transform(img) if self.transform is not None else img
+		assert img.shape[-1] == 240 
+		img_mask = self.target_transform(
+			img_mask) if self.target_transform is not None and img_mask is not None else img_mask
+		img_mask = [] if img_mask is None else img_mask
+		return {'img': img, 'img_mask': img_mask, 'cls_name': cls_name, 'anomaly_type': anomaly_type,
+				'img_path': os.path.join(self.root, img_path), 'anomaly': anomaly}
+
+
 import random
 import string
 class RandomWordPrompt(data.Dataset):
